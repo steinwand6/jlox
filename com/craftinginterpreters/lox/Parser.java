d58f6c873a55489a4dd6a5ebf47228a855b2c318
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-11-28 23:24:36 +0900

    feat: Add Parser class for parsing tokens

---
+++ com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,17 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import static com.craftinginterpreters.lox.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+}
