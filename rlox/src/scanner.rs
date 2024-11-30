28d592e78c9655fc6fd0cbbec0d56e8adb3f51d3
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-11-30 23:06:04 +0900

    feat: add Scanner struct for token scanning

---
+++ rlox/src/scanner.rs
@@ -0,0 +1,19 @@
+pub struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    fn new(source: String) -> Self {
+        Self {
+            source,
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+}
