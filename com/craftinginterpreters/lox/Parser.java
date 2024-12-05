976a42ca3e570be03ce2a524416b8082b973ebc3
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-12-05 23:35:03 +0900

    feat: enhance error handling in Parser

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -4,6 +4,9 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Parser {
+    private static class ParseError extends RuntimeException {
+    }
+
     private final List<Token> tokens;
     private int current = 0;
 
@@ -84,6 +87,8 @@ public class Parser {
             consume(RIGHT_PAREN, "Expecte ')' after expression.");
             return new Expr.Grouping(expr);
         }
+
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
@@ -94,6 +99,13 @@ public class Parser {
             }
         }
         return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type))
+            return advance();
+
+        throw error(peek(), message);
     }
 
     private boolean check(TokenType type) {
@@ -118,5 +130,10 @@ public class Parser {
 
     private Token previous() {
         return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
     }
 }
