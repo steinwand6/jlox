c0f1a3cedaba0d728128dda3a9d11b03845c1f3b
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-12-14 21:51:40 +0900

    feat: add support for nested environments in Environment

--- rlox/src/environment.rs
+++ rlox/src/environment.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, rc::Rc};
 
 use crate::{
     token::{Object, Token},
@@ -7,12 +7,21 @@ use crate::{
 
 pub struct Environment {
     values: HashMap<String, Object>,
+    enclosing: Option<Rc<Environment>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Self {
             values: HashMap::new(),
+            enclosing: None,
+        }
+    }
+
+    pub fn new_enclosing(enclosing: Rc<Environment>) -> Self {
+        Self {
+            values: HashMap::new(),
+            enclosing: Some(enclosing),
         }
     }
 
@@ -23,10 +32,13 @@ impl Environment {
     pub fn get(&self, name: &Token) -> Result<Object, LoxRuntimeError> {
         match self.values.get(&name.lexeme) {
             Some(value) => Ok(value.clone()),
-            None => Err(LoxRuntimeError(
-                name.clone(),
-                format!("Undefined variable '{}'.", name.lexeme),
-            )),
+            None => match &self.enclosing {
+                Some(enclosing) => enclosing.get(name),
+                None => Err(LoxRuntimeError(
+                    name.clone(),
+                    format!("Undefined variable '{}'.", name.lexeme),
+                )),
+            },
         }
     }
 
@@ -34,10 +46,18 @@ impl Environment {
         if self.values.contains_key(&name.lexeme) {
             self.values.insert(name.lexeme.clone(), value.clone());
         } else {
-            return Err(LoxRuntimeError(
-                name.clone(),
-                format!("Undefined variable '{}'.", name.lexeme),
-            ));
+            match &self.enclosing {
+                Some(enclosing) => match enclosing.get(name) {
+                    Ok(_) => self.define(&name.lexeme, value),
+                    Err(e) => return Err(e),
+                },
+                None => {
+                    return Err(LoxRuntimeError(
+                        name.clone(),
+                        format!("Undefined variable '{}'.", name.lexeme),
+                    ))
+                }
+            }
         }
         Ok(())
     }
