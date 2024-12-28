68487bcbcab6dcd40aee0895b6ba58a745abce5a
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-12-28 23:58:34 +0900

    feat: add support for return statement

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -87,7 +87,10 @@ public class Parser {
         if (match(IF))
             return ifStatement();
         if (match(PRINT))
+
             return printStatement();
+        if (match(RETURN))
+            return returnStatement();
         if (match(WHILE))
             return whileStatement();
         if (match(LEFT_BRACE))
@@ -153,6 +156,16 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after return value.");
+        return new Stmt.Return(keyword, value);
     }
 
     private Stmt whileStatement() {
